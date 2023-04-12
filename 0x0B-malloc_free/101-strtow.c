9f74aa81abcf614f85e487669dd5205795c9593a
SamPhewa <s.phewa93@gmail.com>
2023-04-12 00:35:12 -0700

    C - malloc, free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -12,25 +12,22 @@ int count_word(char *s)
 
 {
 
-		int flag, c, w;
-			flag = 0;
-				w = 0;
-					for (c = 0; s[c] != '\0'; c++)
-							{
-								if (s[c] == ' ')
-														flag = 0;
-								else if (flag == 0)
+	int flag, c, w;
+		flag = 0;
+		w = 0;
+			for (c = 0; s[c] != '\0'; c++)
+		{
+				if (s[c] == ' ')
+					flag = 0;
+				else if (flag == 0)
 
-															{
+	{
+				flag = 1;
+				w++;
+	}
 
-																		flag = 1;
-
-																						w++;
-																								}
-
-													}
-
-						return (w);
+	}
+		return (w);
 
 }
 
@@ -82,14 +79,11 @@ char **strtow(char *str)
 				k++;
 				c = 0;
 			}
-
 		}
 
 		else if (c++ == 0)
 			start = i;
-
 	}
-
 	matrix[k] = NULL;
 	return (matrix);
 
